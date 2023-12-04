49a134798fd0f232d2b7f4e931533042157ff8eb
Markus Amano <markus.a.amano@gmail.com>
2023-12-04 17:33:14 +0900

    hello world

---
+++ makebibsfromarxivs.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import requests
+import sys
+import os
+import urllib
+
+# inspirehep docs: https://github.com/inspirehep/rest-api-doc
+USAGE = "./makebibsfromarxivs <urlfile.txt>"
+
+
+def getinput():
+    if len(sys.argv) < 2:
+        print("No arguments were given.", file=sys.stderr)
+        print(USAGE, file=sys.stderr)
+        exit(1)
+    return sys.argv[1]
+
+
+def load_urls_from_file(filename: str):
+    if not os.path.isfile(filename):
+        print(f"{filename} does not exists.", file=sys.stderr)
+        exit(1)
+    with open(filename, "r") as file:
+        return [line.strip() for line in file.readlines()]
+
+
+def main():
+    arxivfile = getinput()
+    arxivids = load_urls_from_file(arxivfile)
+    for arxivid in arxivids:
+        r = requests.get(
+            f"https://inspirehep.net/api/arxiv/{arxivid.strip()}?format=bibtex"
+        )
+        if r.status_code >= 300:
+            print("From HEP", r.text, file=sys.stderr)
+            print("Could not file URL for", arxivid, file=sys.stderr)
+            continue
+        print(r.text)
+        print()
+
+
+if __name__ == "__main__":
+    main()
